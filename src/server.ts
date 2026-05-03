8a3a0af51f68f9581f65ac1e9e7c6a21ee2d5281
madruga3035 <professormarcoveio@gmail.com>
2026-05-03 14:02:46 -0300

    atualização

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes); // Aqui ele carrega todas as rotas que definimos
 
 app.listen(PORT, () => {
-  console.log(`🚀 Spektron System rodando na porta ${PORT}`);
+  console.log(`🚀 Spektron System V8 rodando forte na porta ${PORT}`);
 });
