6d6ff6d7b4913db0fe737819d78ee1a79a0bf912
madruga3035 <professormarcoveio@gmail.com>
2026-05-03 13:40:51 -0300

    atualização

--- src/routes.ts
+++ src/routes.ts
@@ -29,25 +29,28 @@ routes.post('/login', authController.authenticate);
 
 // --- Filtro de Autenticação Global ---
 // A partir daqui, o req.userId e req.userRole são injetados automaticamente
-routes.use(authMiddleware);
+//routes.use(authMiddleware);
 
 // --- Rotas Privadas (Protegidas por Cargo) ---
-
 // Somente ADMIN
-routes.get('/admin/users', checkRole(['ADMIN']), userController.index);
+routes.get('/admin/users', authMiddleware, checkRole(['ADMIN']), userController.index);
 // Somente ADMIN pode criar outros usuários (ex: um admin criando um técnico)
-routes.post('/admin/users', checkRole(['ADMIN']), userController.createByAdmin);
+routes.post('/admin/users', authMiddleware, checkRole(['ADMIN']), userController.createByAdmin);
 // Edição de perfil (O controller garante que USER só edite a si mesmo)
-routes.put('/users/:id', userController.update);
+routes.put('/users/:id', authMiddleware,userController.update);
 // Deleção/Desativação (Somente ADMIN)
-routes.delete('/users/:id', checkRole(['ADMIN']), userController.delete);
+routes.delete('/users/:id', authMiddleware, checkRole(['ADMIN']), userController.delete);
 
 // ADMIN e TECHNICIAN
-routes.post('/records', checkRole(['ADMIN', 'TECHNICIAN' , 'USER']), recordController.store);
-routes.get('/records', checkRole(['ADMIN', 'TECHNICIAN', 'USER']), recordController.index);
+routes.post('/records', authMiddleware, checkRole(['ADMIN', 'TECHNICIAN' , 'USER']), recordController.store);
+routes.get('/records', authMiddleware, checkRole(['ADMIN', 'TECHNICIAN', 'USER']), recordController.index);
 // Somente ADMIN pode deletar registros
-routes.delete('/records/:id', checkRole(['ADMIN']), recordController.delete);
+routes.delete('/records/:id', authMiddleware, checkRole(['ADMIN']), recordController.delete);
 // ADMIN e USER podem editar (a lógica dentro do controller filtra quem é o dono)
-routes.put('/records/:id', checkRole(['ADMIN', 'TECHNICIAN', 'USER']), recordController.update);
+routes.put('/records/:id', authMiddleware, checkRole(['ADMIN', 'TECHNICIAN', 'USER']), recordController.update);
+
+routes.use((req, res) => {
+  res.status(404).json({ error: "Rota não encontrada. Verifique o endereço e o método (GET/POST)." });
+});
 
 export { routes };
